8cca57fa1c9a8e99c86feda0a074def788cab897
lauren levaccare <laurenlevaccare@icloud.com>
2020-11-10 20:24:40 -0600

    NOV 10 change

--- script.js
+++ script.js
@@ -1,8 +1,96 @@
-var queryURL = "https://www.googleapis.com/books/v1/volumes?q=978-0553212457"
+* {
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function(response) {
-    console.log(response)
-})
+font-family: 'Cormorant Garamond', serif;
+
+font-size: 20px;
+
+background-color: rgb(247, 243, 236);
+
+text-align: center;
+
+margin-top: 20px;
+
+background-image: url(./imageFolder/thecode1-1.png);
+   
+
+}
+
+.row {
+
+        margin-right: 30px;
+
+}
+
+::placeholder {
+          
+
+}
+
+.subheader {
+
+        font-weight: 80px;
+        color: black;
+        background-image: url(https://www.r-graph-gallery.com/how-to-draw-connecting-routes-on-map-with-r-and-great-circles_files/figure-html/thecode1-1.png);
+        
+      text-emphasis: center;
+      margin-left: 25px;
+    
+
+}       
+
+label {
+
+    
+       font-family: 'Cormorant Garamond', serif; 
+
+        font-size: 35px;
+
+        line-height: .8;
+   
+     
+        
+
+
+
+}
+
+h1 {
+
+       font-family: 'Cormorant Garamond', serif; 
+       margin-left: 20px;
+}
+
+
+.lead {
+ font-family: 'Cormorant Garamond', serif; 
+        background-image: url(./imageFolder/travelbg.jpeg);
+
+
+
+}
+
+
+
+#menu {
+
+        margin-top: 10px;
+}
+
+
+textarea {
+
+        background-image: url(./imageFolder/travelbg.jpeg);
+
+}
+
+.button  {
+
+   background-image: url(./imageFolder/travelbg.jpeg);
+background-color: url(./imageFolder/travelbg.jpeg);
+   color: black;
+
+   font-weight: 30px;
+        font-size: 20px;
+   text-align: justify;
+        
+        }
